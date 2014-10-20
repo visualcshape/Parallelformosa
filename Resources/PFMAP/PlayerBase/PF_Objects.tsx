c9bb75590b7ff5771fa06de356312878ebc44574
longbiau <longbiau@hotmail.com.tw>
2014-10-21 00:58:19 +0900

    Update pictures. (Do not copy)

--- Resources/PFMAP/PlayerBase/PF_Objects.tsx
+++ Resources/PFMAP/PlayerBase/PF_Objects.tsx
@@ -1,46 +1,39 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="PF_Objects" tilewidth="62" tileheight="117" spacing="2" margin="2">
+<tileset name="PF_Objects" tilewidth="101" tileheight="171" spacing="2" margin="2">
  <image source="PF_Objects.png" width="600" height="800"/>
  <tile id="0">
   <properties>
    <property name="buildable" value="1"/>
-   <property name="walkable" value="1"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
    <property name="buildable" value="0"/>
-   <property name="walkable" value="1"/>
   </properties>
  </tile>
  <tile id="2">
   <properties>
    <property name="buildable" value="1"/>
-   <property name="walkable" value="1"/>
   </properties>
  </tile>
  <tile id="3">
   <properties>
    <property name="buildable" value="1"/>
-   <property name="walkable" value="1"/>
   </properties>
  </tile>
  <tile id="4">
   <properties>
    <property name="buildable" value="0"/>
-   <property name="walkable" value="0"/>
   </properties>
  </tile>
  <tile id="5">
   <properties>
    <property name="buildable" value="1"/>
-   <property name="walkable" value="1"/>
   </properties>
  </tile>
  <tile id="6">
   <properties>
    <property name="buildable" value="1"/>
-   <property name="walkable" value="1"/>
   </properties>
  </tile>
 </tileset>
